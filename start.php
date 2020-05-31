79c7c3fc93ccc6f7bdb30bc33fc4ba557f03a314
Ivan Jakab <ivan.jakab@gauss.hr>
2020-05-31 13:02:59 +0200

    chore: modification for docker run

--- start.php
+++ start.php
@@ -1,6 +1,5 @@
 <?php
 
-include_once 'env.php'; // include instead of require as the file might not be present, but env variables could be inserted to project through parent process or cloud service etc.
 require_once 'database.php';
 
 require_once 'entities/repository.interface.php';
